9015bd2ea5d607fc949b6a2b619adb0a703cc9ca
HFO4 <912394456@qq.com>
2019-04-20 22:04:55 +0800

    Feat: Read rom data

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/HFO4/gbc-in-cloud
+
+go 1.12
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/HFO4/gbc-in-cloud/gb"
+
+func main() {
+	core := gb.Core{}
+	core.Init("G:\\LearnGo\\gb\\test.gb")
+}
