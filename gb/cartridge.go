6c50ebc2efcf67032c625c8ffbc7bc4925e205db
HFO4 <912394456@qq.com>
2019-04-21 16:41:23 +0800

    Feat: Parse Rom data

--- gb/cartridge.go
+++ gb/cartridge.go
@@ -1,10 +1,139 @@
 package gb
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
-type Cartridge interface {
+/*
+	00h  ROM ONLY                 13h  MBC3+RAM+BATTERY
+	01h  MBC1                     15h  MBC4
+	02h  MBC1+RAM                 16h  MBC4+RAM
+	03h  MBC1+RAM+BATTERY         17h  MBC4+RAM+BATTERY
+	05h  MBC2                     19h  MBC5
+	06h  MBC2+BATTERY             1Ah  MBC5+RAM
+	08h  ROM+RAM                  1Bh  MBC5+RAM+BATTERY
+	09h  ROM+RAM+BATTERY          1Ch  MBC5+RUMBLE
+	0Bh  MMM01                    1Dh  MBC5+RUMBLE+RAM
+	0Ch  MMM01+RAM                1Eh  MBC5+RUMBLE+RAM+BATTERY
+	0Dh  MMM01+RAM+BATTERY        FCh  POCKET CAMERA
+	0Fh  MBC3+TIMER+BATTERY       FDh  BANDAI TAMA5
+	10h  MBC3+TIMER+RAM+BATTERY   FEh  HuC3
+	11h  MBC3                     FFh  HuC1+RAM+BATTERY
+	12h  MBC3+RAM
+*/
+var cartridgeTypeMap = map[byte]string{
+	byte(0x00): "ROM ONLY",
+	byte(0x01): "MBC1",
+	byte(0x02): "MBC1+RAM",
+	byte(0x03): "MBC1+RAM+BATTERY",
+	byte(0x05): "MBC2",
+	byte(0x06): "MBC2+BATTERY",
+	byte(0x08): "ROM+RAM",
+	byte(0x09): "ROM+RAM+BATTERY",
+	byte(0x0B): "MMM01",
+	byte(0x0C): "MMM01+RAM",
+	byte(0x0D): "MMM01+RAM+BATTERY",
+	byte(0x0F): "MBC3+TIMER+BATTERY",
+	byte(0x10): "MBC3+TIMER+RAM+BATTERY",
+	byte(0x11): "MBC3",
+	byte(0x12): "MBC3+RAM",
+	byte(0x13): "MBC3+RAM+BATTERY",
+	byte(0x15): "MBC4",
+	byte(0x16): "MBC4+RAM",
+	byte(0x17): "MBC4+RAM+BATTERY",
+	byte(0x19): "MBC5",
+	byte(0x1A): "MBC5+RAM",
+	byte(0x1B): "MBC5+RAM+BATTERY",
+	byte(0x1C): "MBC5+RUMBLE",
+	byte(0x1D): "MBC5+RUMBLE+RAM",
+	byte(0x1E): "MBC5+RUMBLE+RAM+BATTERY",
+	byte(0xFC): "POCKET CAMERA",
+	byte(0xFD): "BANDAI TAMA5",
+	byte(0xFE): "HuC3",
+	byte(0x1F): "HuC1+RAM+BATTERY",
+}
+
+/*
+	ROM bank number is linked to the ROM Size byte (0148).
+		1 bank = 16 KBytes
+	0x00 means no bank required.
+*/
+var RomBankMap = map[byte]uint8{
+	byte(0x00): 2,
+	byte(0x01): 4,
+	byte(0x02): 8,
+	byte(0x03): 16,
+	byte(0x04): 32,
+	byte(0x05): 64,
+	byte(0x06): 128,
+	byte(0x52): 72,
+	byte(0x53): 80,
+	byte(0x54): 96,
+}
+
+/*
+	RAM bank number is linked to the RAM Size byte (0149).
+		1 bank = 8 KBytes
+	0x00 means no bank required.
+*/
+var RamBankMap = map[byte]uint8{
+	byte(0x00): 0,
+	byte(0x01): 1,
+	byte(0x02): 1,
+	byte(0x03): 4,
+}
+
+type Cartridge struct {
+	Props CartridgeProps
+	MBC   MBC
+}
+
+/*
+	Cartridge props
+*/
+type CartridgeProps struct {
+	MBCType string
+	ROMBank uint8
+	RAMBank uint8
+}
+
+type MBC interface {
+}
+
+/*
+	Single ROM without MBC
+*/
+type MBCRom struct {
+	rom []byte
 }
 
 func InitCartridge() {
 	fmt.Println("ss")
+}
+
+/*
+	Read cartridge data from file
+*/
+func (core *Core) readRomFile(romPath string) []byte {
+	log.Println("[Core] Loading rom file...")
+	romFile, err := os.Open(romPath)
+	defer romFile.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	stats, statsErr := romFile.Stat()
+	if statsErr != nil {
+		log.Fatal(statsErr)
+	}
+	var size int64 = stats.Size()
+	bytes := make([]byte, size)
+
+	bufReader := bufio.NewReader(romFile)
+	_, err = bufReader.Read(bytes)
+
+	log.Printf("[Core] %d Bytes rom loaded\n", size)
+	return bytes
 }
--- gb/core.go
+++ gb/core.go
@@ -1,37 +1,118 @@
 package gb
 
 import (
-	"bufio"
-	"fmt"
 	"log"
-	"os"
 )
 
 type Core struct {
-	Rom Cartridge
+	Cartridge Cartridge
 }
 
 func (core *Core) Init(romPath string) {
-	romData := core.readRomFile(romPath)
-	fmt.Printf("%s", string(romData[0x134:0x143]))
+	core.initRom(romPath)
+	core.initMemory()
 }
 
-//Read cartridge data from file
-func (core *Core) readRomFile(romPath string) []byte {
-	romFile, err := os.Open(romPath)
-	defer romFile.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	stats, statsErr := romFile.Stat()
-	if statsErr != nil {
-		log.Fatal(statsErr)
-	}
-	var size int64 = stats.Size()
-	bytes := make([]byte, size)
+func (core *Core) initMemory() {
+	log.Println("[Core] Start to initialize memory...")
+}
 
-	bufReader := bufio.NewReader(romFile)
-	_, err = bufReader.Read(bytes)
+/*
+	Initialize Cartridge, load rom file and decode rom props
+*/
+func (core *Core) initRom(romPath string) {
+	romData := core.readRomFile(romPath)
 
-	return bytes
+	/*
+		0134-0143 - Title
+
+		Title of the game in UPPER CASE ASCII. If it is less than 16 characters
+		then the remaining bytes are filled with 00's. When inventing the CGB, Nintendo
+		has reduced the length of this area to 15 characters, and some months later they
+		had the fantastic idea to reduce it to 11 characters only. The new meaning of the
+		ex-title bytes is described below.
+	*/
+	log.Printf("[Cartridge] Game title: %s\n", string(romData[0x134:0x143]))
+
+	/*
+		0143 - CGB Flag
+
+		80h - Game supports CGB functions, but works on old gameboys also.
+		C0h - Game works on CGB only (physically the same as 80h).
+	*/
+	isCGB := (romData[0x143] == 0x80 || romData[0x143] == 0xC0)
+	log.Printf("[Cartridge] CGB mode: %t\n", isCGB)
+
+	/*
+		0147 - Cartridge Type
+
+		Specifies which Memory Bank Controller (if any) is used in the cartridge,
+		and if further external hardware exists in the cartridge.
+	*/
+	CartridgeType := romData[0x147]
+	if _, ok := cartridgeTypeMap[CartridgeType]; !ok {
+		log.Fatalf("[Cartridge] Unknown cartridge type: %x\n", CartridgeType)
+	}
+	log.Printf("[Cartridge] Cartridge type: %s\n", cartridgeTypeMap[CartridgeType])
+
+	/*
+		Init Cartridge struct according to cartridge type
+	*/
+	core.Cartridge = Cartridge{}
+	switch CartridgeType {
+	case 0x00, 0x08, 0x09, 0x0B, 0x0C, 0x0D:
+		core.Cartridge.MBC = MBCRom{
+			rom: romData,
+		}
+		core.Cartridge.Props = CartridgeProps{
+			MBCType: "rom",
+		}
+	case 0x01, 0x02, 0x03:
+		log.Println("mbc1")
+	case 0x05, 0x06:
+		log.Println("mbc2")
+	case 0x0F, 0x10, 0x11, 0x12, 0x13:
+		log.Println("mbc3")
+	case 0x15, 0x16, 0x17:
+		log.Println("mbc4")
+	case 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E:
+		log.Println("mbc5")
+	default:
+		log.Fatal("[Cartridge] Unsupported MBC type")
+	}
+
+	/*
+		Get rom ban number according to ROM Size byte (0148)
+		Specifies the ROM Size of the cartridge. Typically calculated as "32KB shl N".
+		  00h -  32KByte (no ROM banking) - here we set the bank number to 2
+		  01h -  64KByte (4 banks)
+		  02h - 128KByte (8 banks)
+		  03h - 256KByte (16 banks)
+		  04h - 512KByte (32 banks)
+		  05h -   1MByte (64 banks)  - only 63 banks used by MBC1
+		  06h -   2MByte (128 banks) - only 125 banks used by MBC1
+		  07h -   4MByte (256 banks)
+		  52h - 1.1MByte (72 banks)
+		  53h - 1.2MByte (80 banks)
+		  54h - 1.5MByte (96 banks)
+	*/
+	if _, ok := RomBankMap[romData[0x148]]; !ok {
+		log.Fatalf("[Cartridge] Unknown ROM size byte : %x\n", romData[0x148])
+	}
+	core.Cartridge.Props.ROMBank = RomBankMap[romData[0x148]]
+	log.Printf("[Cartridge] ROM bank number: %d (%dKBytes)\n", core.Cartridge.Props.ROMBank, core.Cartridge.Props.ROMBank*16)
+
+	/*
+		Get rom ban number according to ROM Size byte (0149)
+		Specifies the size of the external RAM in the cartridge (if any).
+		  00h - None		(0 banks of 8KBytes each)
+		  01h - 2 KBytes	(1 banks of 8KBytes each)
+		  02h - 8 KBytes	(1 banks of 8KBytes each)
+		  03h - 32 KBytes 	(4 banks of 8KBytes each)
+	*/
+	if _, ok := RamBankMap[romData[0x149]]; !ok {
+		log.Fatalf("[Cartridge] Unknown RAM size byte : %x\n", romData[0x149])
+	}
+	core.Cartridge.Props.RAMBank = RamBankMap[romData[0x148]]
+	log.Printf("[Cartridge] RAM bank number: %d (%dKBytes)\n", core.Cartridge.Props.RAMBank, core.Cartridge.Props.RAMBank*8)
 }
